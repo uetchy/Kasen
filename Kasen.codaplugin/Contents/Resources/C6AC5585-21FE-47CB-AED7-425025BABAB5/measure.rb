f71940ff915b08c801314d0ae6d73ace29e39286
Yasuaki Uechi <uetchy@randompaper.co>
2012-08-03 15:54:18 +0900

    fixed

--- Kasen.codaplugin/Contents/Resources/C6AC5585-21FE-47CB-AED7-425025BABAB5/measure.rb
+++ Kasen.codaplugin/Contents/Resources/C6AC5585-21FE-47CB-AED7-425025BABAB5/measure.rb
@@ -4,7 +4,7 @@
 identify_path = "/usr/local/bin/identify"
 
 line = STDIN.gets
-space = line.scan(/^(\s+?)\w/)[0][0]
+space = line.scan(/^(\s+?)\w/)[0][0] rescue ""
 rel_path = line.scan(/url\("?(.+?)"?\)/)[0][0]
 
 image_path = File.expand_path(File.join(File.dirname(ENV["CODA_FILEPATH"]), rel_path))
@@ -13,5 +13,10 @@ image_ext = File.extname(image_path)
 image_dir = File.dirname(image_path)
 
 width, height =  `#{identify_path} -verbose "#{image_path}" | grep Geometry | awk '{print $2}'`.strip.split(/x|\+/)
-puts "\n#{space}width: #{width}px;"
+unless(line.include?("background:"))
+  puts "background: url(#{rel_path});"
+else
+  print "\n"
+end
+puts "#{space}width: #{width}px;"
 print "#{space}height: #{height}px;"
